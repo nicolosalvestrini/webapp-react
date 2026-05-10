e53923a2c436650fddc8726475feb00cc73af8cf
nicolosalvestrini <nicolo.salvestrini@gmail.com>
2026-05-10 22:17:32 +0200

    add in header link from home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
+import { NavLink } from "react-router-dom";
+
 function Header() {
   return (
-    <header className="py-4 bg-black">
-      <div className="container text-center">
-        <h1 className="text-white fw-bold ">CINEVERSE</h1>
+    <header className="py-4 bg-black border-bottom border-secondary">
+      <div className="container d-flex align-items-center justify-content-between">
+        <h1 className="text-white fw-bold m-0 fs-3">CINEVERSE</h1>
+
+        <nav>
+          <NavLink to="/" className="nav-link-custom">
+            Home
+          </NavLink>
+        </nav>
       </div>
     </header>
   );
